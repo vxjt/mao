91253be60f5210c4f938f9481c7557c086d31cc4
vxjt <nsandborn@gmail.com>
2022-10-17 10:14:44 -0500

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,94 +1,104 @@
-//if rendering is weird the camera is at 0.
+//import * as THREE from 'three';
+import { Color, Vector3, OrthographicCamera, Scene, WebGLRenderer, Line, LineBasicMaterial, BufferGeometry, Mesh, MeshBasicMaterial, ShapeGeometry, DoubleSide} from 'three';
 
-'use strict';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 
-import * as THREE from 'three';
-
-//import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js';
-//import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-
-
-let line;
+import threehelv from './helvetiker_bold.typeface.json';
 
 const chart = document.querySelector('#chart') as HTMLElement;
 const search = document.querySelector('#search') as HTMLElement;
 const style = getComputedStyle(document.body)
-const colora = new THREE.Color(style.getPropertyValue('background-color'))
-const colorb = new THREE.Color(style.getPropertyValue('color'))
+const colora = new Color(style.getPropertyValue('background-color'))
+const colorb = new Color(style.getPropertyValue('color'))
 
-const scene = new THREE.Scene();
-
-const renderer = new THREE.WebGLRenderer({
-    canvas: chart,
-    antialias: true
-});
-
-//set up camera & scene
-const camera = new THREE.OrthographicCamera(0, chart.clientWidth, chart.clientHeight, 0, 0, 1);
-//l, t, -n --- r, t, -n
+//(l, r, t, b), l>r x, b>t y.
+//l, t, -n -- r, t, -n
 //   |           |
 //l, b, -n -- r, b, -n
-//l>r x
-//b>t y
+const camera = new OrthographicCamera(0, chart.clientWidth, chart.clientHeight, 0, 0, 1);
+
+const scene = new Scene();
+
+const renderer = new WebGLRenderer({
+  canvas: chart,
+  antialias: true
+});
+
+const linemat = new LineBasicMaterial({ color: colorb });
+const linebg = new BufferGeometry()
+
+const line = new Line(linebg, linemat);
+
+const font = new FontLoader().parse(threehelv);
+
+const textmat = new MeshBasicMaterial({
+  color: colorb,
+  side: DoubleSide
+});
+
+const text = new Mesh(new BufferGeometry(), textmat);
 
 init();
 
 animate();
 
 function init() {
-    resizeCanvasToDisplaySize();
+  resizeCanvasToDisplaySize();
 
-    //create geometry
-    const linebg = new THREE.BufferGeometry();
-    const linemat = new THREE.LineBasicMaterial({ color: colorb });
+  scene.add(line);
+  scene.add(text);
 
-    const points = [];
+  scene.background = colora;
 
-    for (let i = 0; i <= 50; i++) {
-        const point = new THREE.Vector3();
-        point.x = i / 50 * camera.right * 2;
-        point.y = Math.random() * camera.top * 2;
-        point.z = 0;
-        points.push(point.x, point.y, point.z);
-    }
-
-    console.log(points);
-
-    linebg.setAttribute('position', new THREE.Float32BufferAttribute(points, 3));
-    line = new THREE.Line(linebg, linemat);
-    scene.add(line);
-    scene.background = colora;
-
-    //hook events
-    window.addEventListener('resize', onWindowResize);
-    search.addEventListener('keydown', bigsearch);
+  window.addEventListener('resize', onWindowResize);
+  search.addEventListener('keydown', bigsearch);
 }
 
 function onWindowResize() {
-    resizeCanvasToDisplaySize();
+  resizeCanvasToDisplaySize();
 }
 
-function bigsearch(event:any) {
-    if (event.key == "Enter"){
-        let queryurl = `/cf/${event.target.value}`
-        fetch(queryurl)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-    }
+function bigsearch(event: any) {
+  if (event.key == "Enter") {
+    let queryurl = `/cf/${event.target.value}`
+    fetch(queryurl)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  } else if (event.key == "ArrowUp") {
+    updatetext(event.target.value)
+  } else if (event.key == "ArrowDown") {
+    updatelines()
+    console.log(scene)
+  }
+}
+
+function updatetext(msg:string) {
+  const textshape = font.generateShapes(msg, 20);
+  const textgeo = new ShapeGeometry(textshape);
+  text.geometry = textgeo
+}
+
+function updatelines() {
+  //update only vertexes
+  let linepoints = [];
+  linepoints.push( new Vector3(0, Math.random() * 150, 0) );
+  linepoints.push( new Vector3(400, Math.random() * 200, 0 ) );
+  linepoints.push( new Vector3(800, Math.random() * 250, 0 ) );
+  linebg.setFromPoints(linepoints)
 }
 
 function render() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 function animate() {
-    requestAnimationFrame(animate);
-    render();
+  requestAnimationFrame(animate);
+  render();
 }
 
 function resizeCanvasToDisplaySize() {
-    camera.updateProjectionMatrix(); //updates camera settings
-    renderer.setSize(chart.clientWidth, chart.clientHeight);
+  camera.updateProjectionMatrix(); //updates camera settings
+  renderer.setSize(chart.clientWidth, chart.clientHeight);
 }
 
 //-128 to 127 Int8Array
