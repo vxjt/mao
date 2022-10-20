afbc32610114b3febab053bafbc96b9e3fec812f
vxjt <nsandborn@gmail.com>
2022-10-20 09:47:55 -0500

    wip on charting

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
-//import * as THREE from 'three';
-import { Color, Vector3, OrthographicCamera, Scene, WebGLRenderer, Line, LineBasicMaterial, BufferGeometry, Mesh, MeshBasicMaterial, ShapeGeometry, DoubleSide} from 'three';
+import { Color, OrthographicCamera, Scene, WebGLRenderer, Mesh, MeshBasicMaterial, ShapeGeometry, DoubleSide, Vector4 } from 'three';
+
+import { Vector3, Line, LineBasicMaterial, BufferGeometry } from 'three'; //line drawing
 
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 
 import threehelv from './helvetiker_bold.typeface.json';
 
-import { testdata } from './test'
+import { maketestdata, boxall, debugsquare } from './test'
+
+export type Cdata = {
+  entityName: string
+  operationName: string
+  startDate: string
+  values: {
+    [key: number]: number
+  }
+}
 
 let canvaswidth: number, canvasheight: number;
 
-let drawsettings = {'margin': 15}
+let drawsettings = {
+  'margin': 12,
+  'digits': 3
+}
 
 const chart = document.querySelector('#chart') as HTMLElement;
 const search = document.querySelector('#search') as HTMLElement;
@@ -31,9 +44,6 @@ const renderer = new WebGLRenderer({
 });
 
 const linemat = new LineBasicMaterial({ color: colorb });
-const linebg = new BufferGeometry()
-
-const line = new Line(linebg, linemat);
 
 const font = new FontLoader().parse(threehelv);
 
@@ -42,21 +52,16 @@ const textmat = new MeshBasicMaterial({
   side: DoubleSide
 });
 
-const text = new Mesh(new BufferGeometry(), textmat);
-
 init();
 
 animate();
 
 function init() {
-  scene.add(line);
-  scene.add(text);
-
-  scene.background = colora;
+  scene.background = colora
 
   window.addEventListener('resize', onWindowResize);
   search.addEventListener('keydown', bigsearch);
-  
+
   onWindowResize()
 }
 
@@ -66,47 +71,9 @@ function onWindowResize() {
   resizeCanvasToDisplaySize();
 }
 
-const testx = Math.random() * 100 //2015-09-26
-const testy = Math.floor(Math.random() * 100000000000) //
-const testsf = Math.floor(Math.random() * 100)
-
-function testmethod1() {
-  let errorvar
-  for(let i =0 ; i < 10000; i++) {
-    let height = testy + testsf * 3 + (canvasheight % testsf) / (Math.floor(canvasheight / (testy + testsf * 3)))
-    let width = testx + testsf * 4 + (canvaswidth % testsf) / (Math.floor(canvaswidth / (testx + testsf * 4)))
-    errorvar =+ height + width
-//y label height: y + sf * 3 + (chart.clientheight % sf) / (floor(chart.clientheight / (y + sf * 3)))
-//x label width: x + sf * 4 + chart.clientwidth % sf / (floor(chart.clientwidth / (x + sf * 4)))
- }
- return errorvar
-}
-
-/*
-function testmethod2() {
-  let errorvar
-  for(let i =0 ; i < 10000; i++) {
-    let height = testy + testsf * 3 + (canvasheight % testsf) / (newfloor(canvasheight / (testy + testsf * 3)))
-    let width = testx + testsf * 4 + (canvaswidth % testsf) / (newfloor(canvaswidth / (testx + testsf * 4)))
-    errorvar =+ height + width
-//y label height: y + sf * 3 + (chart.clientheight % sf) / (floor(chart.clientheight / (y + sf * 3)))
-//x label width: x + sf * 4 + chart.clientwidth % sf / (floor(chart.clientwidth / (x + sf * 4)))
- }
- return errorvar
-}
-
-console.time();
-console.log(testmethod1())
-console.timeEnd();
-console.time();
-//console.log(testmethod2())
-console.timeEnd();
-
-*/
-
-function bigsearch(event:KeyboardEvent) {
+function bigsearch(event: KeyboardEvent) {
   if (event.key == "Enter") {
-    drawchart(testdata)
+    drawchart(maketestdata())
     /*
     let queryurl = `/cf/${event.target.value}`
     fetch(queryurl)
@@ -116,43 +83,195 @@ function bigsearch(event:KeyboardEvent) {
   }
 }
 
-function drawchart(data:Object) {
-  console.log(data, drawsettings)
+function drawchart(data: Cdata) {
+  
+  scene.clear()
+
+  let keyarraystr = Object.keys(data.values)
+  let keyarray = keyarraystr.map(key => {
+    return Number(key);
+  });
+
+  let valuesarray = Object.values(data.values)
+
+  let minx = Math.min(...keyarray)
+  let maxx = Math.max(...keyarray)
+
+  let miny = Math.min(...valuesarray)
+  let maxy = Math.max(...valuesarray)
+
+  let startdate = Date.parse(data.startDate)
+
+  let minxdate = startdate + minx * 24 * 60 * 60 * 1000
+  let maxxdate = startdate + maxx * 24 * 60 * 60 * 1000
+
+  let maxxdatestring = new Intl.DateTimeFormat(undefined, { dateStyle: 'medium' }).format(maxxdate)
+
+  let maxxdateshape = font.generateShapes(maxxdatestring, 10)
+  let maxxdategeo = new ShapeGeometry(maxxdateshape)
+  let maxxmesh = new Mesh(maxxdategeo, textmat);
+
+  let minxdatestring = new Intl.DateTimeFormat(undefined, { dateStyle: 'medium' }).format(minxdate)
+
+  let minxdateshape = font.generateShapes(minxdatestring, 10)
+  let minxdategeo = new ShapeGeometry(minxdateshape)
+  let minxmesh = new Mesh(minxdategeo, textmat)
+
+  let maxy2 = ceil2(maxy, drawsettings.digits)
+
+  let maxy2shape = font.generateShapes(maxy2.toLocaleString(), 10)
+  let maxy2geo = new ShapeGeometry(maxy2shape)
+
+  let maxy2mesh = new Mesh(maxy2geo, textmat)
+
+  let miny2 = floor2(miny, drawsettings.digits)
+
+  let miny2shape = font.generateShapes(miny2.toLocaleString(), 10)
+  let miny2geo = new ShapeGeometry(miny2shape)
+  let miny2mesh = new Mesh(miny2geo, textmat)
+
+  let charttitle = `${data.entityName}\n${data.operationName}`
+
+  let charttitleshape = font.generateShapes(charttitle, 16)
+  let charttitlegeo = new ShapeGeometry(charttitleshape)
+
+  let charttitlemesh = new Mesh(charttitlegeo, textmat)
+
+  let chartcoords = new Vector4()
+
+  let dateaxis = new Vector4()
+
+  let xentries
+
+  minxdategeo.computeBoundingBox()
+  maxxdategeo.computeBoundingBox()
+  miny2geo.computeBoundingBox()
+  maxy2geo.computeBoundingBox()
+  charttitlegeo.computeBoundingBox()
+
+  if (maxxdategeo.boundingBox) {
+
+    maxxmesh.position.x = canvaswidth - maxxdategeo.boundingBox.max.x - drawsettings.margin
+
+    if(miny2geo.boundingBox) {
+
+      chartcoords.y = maxxdategeo.boundingBox.max.y + drawsettings.margin * 2 + (miny2geo.boundingBox.max.y - miny2geo.boundingBox.min.y) / 2
+    
+    }
+
+    miny2mesh.position.y = maxxdategeo.boundingBox.max.y + drawsettings.margin * 2
+    chartcoords.z = canvaswidth - drawsettings.margin - (maxxdategeo.boundingBox.max.x - maxxdategeo.boundingBox.min.x) / 2
+    
+  }
+
+  if (charttitlegeo.boundingBox) {
+
+    maxy2mesh.position.y = canvasheight - drawsettings.margin - charttitlegeo.boundingBox.max.y
+    charttitlemesh.position.y = canvasheight - drawsettings.margin - charttitlegeo.boundingBox.max.y
+
+    if (maxy2geo.boundingBox) {
+
+      if (minxdategeo.boundingBox) {
+  
+        chartcoords.x = maxy2geo.boundingBox.max.x + drawsettings.margin * 2 + (minxdategeo.boundingBox.max.x - minxdategeo.boundingBox.min.x) / 2
+    
+      }
+  
+      charttitlemesh.position.x = maxy2geo.boundingBox.max.x + drawsettings.margin * 2
+      minxmesh.position.x = maxy2geo.boundingBox.max.x + drawsettings.margin * 2
+      
+      chartcoords.w = canvasheight - drawsettings.margin + (maxy2geo.boundingBox.max.y - maxy2geo.boundingBox.min.y) / 2 - charttitlegeo.boundingBox.max.y
+  
+    }
+
+  }
+
+  if (maxxdategeo.boundingBox && maxy2geo.boundingBox && minxdategeo.boundingBox) {
+    
+    dateaxis.x = maxy2geo.boundingBox.max.x + drawsettings.margin * 2 + (minxdategeo.boundingBox.max.x - minxdategeo.boundingBox.min.x) / 2
+    dateaxis.z = canvaswidth - drawsettings.margin - (maxxdategeo.boundingBox.max.x - maxxdategeo.boundingBox.min.x) / 2
+    xentries = Math.floor((dateaxis.z - dateaxis.x) / (Math.max(minxdategeo.boundingBox.max.x, maxxdategeo.boundingBox.max.x) + drawsettings.margin * 5))
+  }
+
+  dateaxis.y = drawsettings.margin
+  dateaxis.w = drawsettings.margin
+  
+  minxmesh.position.y = drawsettings.margin
+  maxxmesh.position.y = drawsettings.margin
+  maxy2mesh.position.x = drawsettings.margin
+  miny2mesh.position.x = drawsettings.margin
+
+  let chartpoints = []
+
+  for (let key in data.values) {
+
+    chartpoints.push(new Vector3(((Number(key) - minx) / (maxx - minx)) * (chartcoords.z - chartcoords.x) + chartcoords.x, ((data.values[key] - miny2) / (maxy2 - miny2)) * (chartcoords.w - chartcoords.y) + chartcoords.y, 0))
+
+  }
+
+  let chartbg = new BufferGeometry().setFromPoints(chartpoints)
+
+  let chartline = new Line(chartbg, linemat)
+
+  if (!xentries) {
+    xentries = 0
+  }
+
+  
+  scene.add(maxxmesh);
+  scene.add(minxmesh);
+
+  scene.add(maxy2mesh)
+  scene.add(miny2mesh)
+
+  scene.add(charttitlemesh)
+
+  //boxall(scene)
+
+  scene.add(chartline);
+
+
+  for (let n = xentries; n > 0; n--) {
+    let progressx = n / (xentries + 1)
+    let midx = progressx * (dateaxis.z - dateaxis.x) + dateaxis.x
+    let curx = progressx * (maxx - minx) + minx
+    let curdate = startdate + curx * 24 * 60 * 60 * 1000
+
+    let curdatestring = new Intl.DateTimeFormat(undefined, { dateStyle: 'medium' }).format(curdate)
+
+    let curdateshape = font.generateShapes(curdatestring.toString(), 10)
+
+    let curdategeo = new ShapeGeometry(curdateshape)
+
+    let curmesh = new Mesh(curdategeo, textmat)
+
+    curdategeo.computeBoundingBox()
+    
+    if(curdategeo.boundingBox) {
+      curmesh.position.x = midx - (curdategeo.boundingBox.max.x - curdategeo.boundingBox.min.x) / 2
+      curmesh.position.y = drawsettings.margin
+    }
+
+    scene.add(curmesh)
+    //debugsquare(new Vector4(midx, 0, midx, 20), scene)
+
+  }
+
+  //debugsquare(chartcoords, scene)
+  //debugsquare(new Vector4(0, 0, canvaswidth, canvasheight), scene)
+  //debugsquare(dateaxis, scene)
 }
 
-//Text needs to be done before lines.  Sizing and alignment of text is much more involved than geometry.
-//operation name
-//company
-//y values
-//x values
-//more x and y values will be supplied than needs to be rendered, so we need to decide what sample to render as well.
-//first problem: given an amount of x y values what do you decide to show
-//get x range
-//get y range
-//available element size
-//y original height = textgeo.boundingBox.max.y - textgeo.boundingBox.min.y
-//y original width = textgeo.boundingBox.max.x - textgeo.boundingBox.min.x
-//y label height: y + sf * 3 + (chart.clientheight % sf) / (floor(chart.clientheight / (y + sf * 3)))
-//x label width: x + sf * 4 + chart.clientwidth % sf / (floor(chart.clientwidth / (x + sf * 4)))
-//---
-//get y range
+function ceil2(n: number, d: number) {
 
+  return Math.ceil(n / Math.pow(10, Math.floor(Math.log10(Math.abs(n))) + 1 - d)) * Math.pow(10, Math.floor(Math.log10(Math.abs(n))) + 1 - d)
 
-function updatetext(msg:string) {
-  const textshape = font.generateShapes(msg, 20);
-  const textgeo = new ShapeGeometry(textshape);
-  textgeo.computeBoundingBox()
-  console.log(textgeo.boundingBox)
-  text.geometry = textgeo
 }
 
-function updatelines() {
-  //update only vertexes
-  let linepoints = [];
-  linepoints.push( new Vector3(0, Math.random() * 150, 0) );
-  linepoints.push( new Vector3(400, Math.random() * 200, 0 ) );
-  linepoints.push( new Vector3(800, Math.random() * 250, 0 ) );
-  linebg.setFromPoints(linepoints)
+function floor2(n: number, d: number) {
+
+  return Math.floor(n / Math.pow(10, Math.floor(Math.log10(Math.abs(n))) + 1 - d)) * Math.pow(10, Math.floor(Math.log10(Math.abs(n))) + 1 - d)
+
 }
 
 function render() {
@@ -160,7 +279,6 @@ function render() {
 }
 
 function animate() {
-  //updatelines()
   requestAnimationFrame(animate);
   render();
 }
--- src/test.ts
+++ src/test.ts
@@ -1,27 +1,48 @@
+import { BoxHelper, Scene, Vector3, BufferGeometry, Line, LineBasicMaterial, Vector4 } from "three"
 import { generateUUID } from "three/src/math/MathUtils"
+import { Cdata } from './main'
 
-export var testdata: any = {}
-
-maketestdata()
-
-function maketestdata() {
-
-    testdata.entityName = generateUUID()
-    testdata.operationName = generateUUID()
-    testdata.values = {}
-    
-    for (let d = Math.ceil(Math.random() * 1000); d > 0; d--) {
-        testdata.values[maketestdate()] = Math.floor(Math.random() * 1000000000000)
+export function maketestdata() {
+    let testdata: Cdata = {
+        entityName: generateUUID(),
+        operationName: generateUUID(),
+        startDate: maketestdate(),
+        values: {}
     }
+
+    for (let d = Math.ceil(Math.random() * 800); d > 0; d = d - Math.ceil(Math.random() * 3)) {
+        testdata.values[d] = Math.floor(Math.random() * 1000000000000)
+    }
+
+    return testdata
 
 }
 
 function maketestdate() {
-    //2009-09-26
     const ryear = 1800 + Math.ceil(Math.random() * 350)
     const rmonth = Math.ceil(Math.random() * 12)
     const rday = Math.ceil(Math.random() * 28)
-    //let tdate = new Date(Date.UTC(ryear, rmonth, rday))
-    //let stringtest:string = new Intl.DateTimeFormat(undefined, { dateStyle: 'medium'}).format(tdate)
     return `${ryear}-${rmonth.toString().padStart(2, '0')}-${rday.toString().padStart(2, '0')}`
+}
+
+export function boxall(sc: Scene) {
+    for (let bg in sc.children) {
+        let box = new BoxHelper(sc.children[bg], 0xffff00)
+        sc.add(box)
+    }
+}
+
+export function debugsquare(c: Vector4, sc: Scene) {
+    let linepoints = [];
+
+    linepoints.push(new Vector3(c.x, c.y, 0))
+    linepoints.push(new Vector3(c.x, c.w, 0));
+    linepoints.push(new Vector3(c.z, c.w, 0));
+    linepoints.push(new Vector3(c.z, c.y, 0));
+    linepoints.push(new Vector3(c.x, c.y, 0))
+
+    let borderbg = new BufferGeometry().setFromPoints(linepoints)
+
+    let border = new Line(borderbg, new LineBasicMaterial({ color: 0x0000ff }))
+    sc.add(border)
 }
